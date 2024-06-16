5b3f051c404a5af46a1418d4beb5f965e88dab40
Markus Byremo <markuswb77@gmail.com>
2024-06-16 20:19:23 +0200

    added clear state icon for search

--- src/views/HomeSearch.tsx
+++ src/views/HomeSearch.tsx
@@ -2,8 +2,13 @@ import { useState } from 'react';
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import ListStock from '../component/ListStock';
 import { StockData } from '../types';
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import ClearIcon from '@mui/icons-material/Clear';
 
-const fetchStockData = async (): Promise<StockData[]> => {
+const fetchStockData = async (query: string): Promise<StockData[]> => {
   const apiUrl = import.meta.env.VITE_API_URL;
   const apiKey = import.meta.env.VITE_API_KEY;
 
@@ -11,7 +16,7 @@ const fetchStockData = async (): Promise<StockData[]> => {
     throw new Error('API key is missing');
   }
 
-  const response = await fetch(`${apiUrl}search?query=Apple&apikey=${apiKey}`);
+  const response = await fetch(`${apiUrl}search?query=${query}&apikey=${apiKey}`);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
@@ -19,19 +24,51 @@ const fetchStockData = async (): Promise<StockData[]> => {
 };
 
 const HomeSearch = () => {
+  const [searchQuery, setSearchQuery] = useState('');
   const [showList, setShowList] = useState(false);
-  const { data, error, isLoading }: UseQueryResult<StockData[], Error> = useQuery({
-    queryKey: ['stockData'],
-    queryFn: fetchStockData,
+
+  const { data, error, isLoading, refetch }: UseQueryResult<StockData[], Error> = useQuery({
+    queryKey: ['stockData', searchQuery],
+    queryFn: () => fetchStockData(searchQuery),
+    enabled: false
   });
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      setShowList(true);
+      refetch();
+    }
+  };
+
+  const handleClear = () => {
+    setSearchQuery('');
+    setShowList(false);
+  };
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
   return (
     <div>
-      <p>Home Search view</p>
-      <button onClick={() => setShowList(true)}>View Stock List</button>
+      <TextField
+        label="Search for Stock..."
+        variant="outlined"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        onKeyDown={handleKeyDown}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              {searchQuery && (
+                <IconButton onClick={handleClear} edge="end">
+                  <ClearIcon />
+                </IconButton>
+              )}
+              <SearchIcon />
+            </InputAdornment>
+          )
+        }}
+      />
       {showList && data && <ListStock data={data} />}
     </div>
   );
